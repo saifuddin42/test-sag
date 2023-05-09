10141e98d1465d5bcfebe4ed5232d40c9aab3ec3
Administrator <Administrator>
2023-05-09 17:41:18 -0400

    Commit made by:Administrator

---
+++ code/source/saif_folder.java
@@ -0,0 +1,38 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class saif_folder
+
+{
+	// ---( internal utility methods )---
+
+	final static saif_folder _instance = new saif_folder();
+
+	static saif_folder _newInstance() { return new saif_folder(); }
+
+	static saif_folder _cast(Object o) { return (saif_folder)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void new_javaService (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(new_javaService)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
